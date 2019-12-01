62ea20cfc3e564a8275a548d32b52a603ec2c0dc
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-12-01 19:46:16 +0100

    format App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store/store';
 
 const App = () => {
-   return (
-      <Provider store={store}>
-         <div className="App">
-
-         </div>
-      </Provider>
-   );
+	return (
+		<Provider store={store}>
+			<div className="App" />
+		</Provider>
+	);
 };
 
 export default App;
