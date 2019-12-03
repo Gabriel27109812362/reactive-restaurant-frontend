46a4d7171ab60c06cd484a0fa0a48240e6251608
MKiezik <michal.kiezik@gmail.com>
2019-12-03 18:31:58 +0100

    Wprowadzenie kilku zmian stylistycznych w plikach Home.js i Home.module.scss

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import styles from './Home.module.scss';
 
 export default class Home extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div id={styles.top}>
-          <ul id={styles.nav_bar}>
-            <li><a href="/src/components/Home/Home.js">Home</a></li>
-            <li><a href="/src/components/About/About.js">About</a></li>
-            <li><a href="#">Sign up</a></li>
-            <li><a href="#">Sign in</a></li>
+      <>
+        <div className={styles.top}>
+          <ul className={styles.nav_bar}>
+            <li className={styles.nav_bar_li}><a className={styles.nav_bar_a} href="/src/components/Home/Home.js">Home</a></li>
+            <li className={styles.nav_bar_li}><a className={styles.nav_bar_a} href="/src/components/About/About.js">About</a></li>
+            <li className={styles.nav_bar_li}><a className={styles.nav_bar_a} href="#">Sign up</a></li>
+            <li className={styles.nav_bar_li}><a className={styles.nav_bar_a} href="#">Sign in</a></li>
           </ul>
         </div>
-        <div id={styles.image}>
+
+        <div className={styles.image}>
 
         </div>
-        <div id={styles.app_name}>
+
+        <div className={styles.app_name}>
             <h1>Reactive Restaurant</h1>
         </div>
 
-
-
-        <div id={styles.bottom}>
+        <div className={styles.bottom}>
 
         </div>
 
-      </React.Fragment>
+      </>
     )
   }
 }
