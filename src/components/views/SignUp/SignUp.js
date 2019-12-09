18a43c74adcefb2d439ffc5ff00237ddfa8b65da
Agnieszka Ignatowicz <agnieszkaignatowicz98@gmail.com>
2019-12-09 19:00:58 +0100

    Rejestracja uzytkownika

--- src/components/views/SignUp/SignUp.js
+++ src/components/views/SignUp/SignUp.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './SignUp.module.scss';
 
-const SignUp = () => {
-  return (
-    <div>
-      SignUp
-    </div>
-  );
-};
+export default class SignUp extends Component {
+  
+  render() {
 
-export default SignUp;
+    return (
+      <>
+        <div className={styles.top}>
+          <div className={styles.nav_bar}>
+            Reactive Restaurant
+          
+          </div>
+        
+        </div>
+        <div className={styles.main}> 
+          <p className={styles.sign_up}>
+            Sign Up  </p>
+            <div className={styles.wrapper}>
+            <form>
+
+            <input type="text" placeholder="Name" name="name" required />
+            <p></p>
+            <input type="text" placeholder="Surname" name="surname" required />
+            <p></p>
+            <input type="email" placeholder="Email" name="email" required />
+            <p></p>
+            <input type="password" placeholder="Password" name="psw" required />
+            <p></p>
+            <input type="password" placeholder="Confirm password" name="psw" required />
+            
+
+          </form>
+          <p></p>
+          <p></p>
+          <button className={styles.submit_button}> Submit </button>
+           </div>
+         
+        
+         </div>
+        <div className={styles.bottom}>
+        </div>
+      </>
+    );
+  }
+}
+
+
+
+
