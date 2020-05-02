7e8babe30b77044668c5e3853b052968f624c7c8
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2020-05-02 13:33:40 +0200

    Add logo to Home page

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -2,19 +2,18 @@ import React from 'react';
 import styles from './Home.module.scss';
 import {NavbarWithMenu} from '../../common/Navbar/Navbar';
 import {Footer} from '../../common/Footer/Footer';
+import logo from '../../../assets/logo.png';
 
 const Home = props => {
 
   return (
     <>
       <NavbarWithMenu/>
-      <div className={styles.image}>
-      </div>
       <div className={styles.appName}>
+        <img className={styles.logo} src={logo} alt="logo"/>
         <h1 className={styles.title}>Reactive Restaurant</h1>
       </div>
       <a href="/aftersignin"><p>Link do komponentu aftersignin</p></a>
-
       <Footer/>
     </>
   );
